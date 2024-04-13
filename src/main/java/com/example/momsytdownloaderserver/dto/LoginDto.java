e35ddb0a88b523ae25997565d1f3f954d68e12ff
unkwn <lsjc12911@naver.com>
2024-04-13 21:28:53 +0900

    feat: implement custom error and exceptions
    - remove dto unused getters

--- src/main/java/com/example/momsytdownloaderserver/dto/LoginDto.java
+++ src/main/java/com/example/momsytdownloaderserver/dto/LoginDto.java
@@ -14,15 +14,7 @@ public class LoginDto {
         return username;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
