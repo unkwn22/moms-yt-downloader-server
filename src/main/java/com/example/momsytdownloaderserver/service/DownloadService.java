e45fb610d7ad393edb2beb9880296435e5d6e51e
unkwn <lsjc12911@naver.com>
2024-04-14 09:31:23 +0900

    feat: Rename and remove
    - Rename and remove logic

--- src/main/java/com/example/momsytdownloaderserver/service/DownloadService.java
+++ src/main/java/com/example/momsytdownloaderserver/service/DownloadService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 public class DownloadService  {
 
     private final ShellBashUtil shellBashUtil;
+    private String directory = "~/moms-yt-downloader-server/download";
 
     public DownloadService(ShellBashUtil shellBashUtil) {
         this.shellBashUtil = shellBashUtil;
@@ -16,11 +17,25 @@ public class DownloadService  {
     public void ytDownloadLogic(String videoId, RequestEntityCommand entityCommand) {
         String initialCommandBuilder = "yt-dlp " +
                 "-o "  + entityCommand.id() + ".mp3 " +
-                "-P ~/moms-yt-downloader-server/download " +
+                "-P " + directory + " " +
                 "-x --audio-format mp3 " +
                 "https://www.youtube.com/watch?v=" + videoId;
         shellBashUtil.runtime(initialCommandBuilder);
+
+        changeFileName(entityCommand);
+        removePreviousFile(entityCommand);
     }
 
-    // TODO move and rename logic
+    private void changeFileName(RequestEntityCommand entityCommand) {
+        String fileName = entityCommand.originalTitle();
+        if(entityCommand.requestedTitle().isEmpty() || entityCommand.requestedTitle().isBlank()) fileName = entityCommand.requestedTitle();
+
+        String changeNameCommand = "cp " + entityCommand.id() + ".mp3 " + directory + "/" + fileName + ".mp3";
+        shellBashUtil.runtime(changeNameCommand);
+    }
+
+    private void removePreviousFile(RequestEntityCommand entityCommand) {
+        String removeFileCommand = "rm -f " + directory + "/" + entityCommand.id() + ".mp3";
+        shellBashUtil.runtime(removeFileCommand);
+    }
 }
